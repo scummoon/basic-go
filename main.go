4c1e86312b32bc9754de9a9849b4d5db9225e521
Scummoon <beka2231@gmail.com>
2026-05-14 18:33:18 +0500

    tw

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	name = "Bekzat"
-	var age int
-	age = 24
-	fmt.Print(name, age)
+	color := "red"
 
+	if color == "red" {
+		fmt.Println("Стоп")
+	} else if color == "yellow" {
+		fmt.Println("Приготовься")
+	} else if color == "green" {
+		fmt.Println("Езжай")
+	} else {
+		fmt.Println("Неверный сигнал")
+	}
+
+	for i := 10; i < 30; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "Четное")
+		} else {
+			fmt.Println(i, "Нечетное")
+		}
+	}
+	temp := 11
+
+	if temp <= 10 {
+		fmt.Println("Холодно")
+	} else {
+		fmt.Println("Тепло")
+	}
 }
